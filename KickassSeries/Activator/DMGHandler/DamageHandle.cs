3066b19e5c59df8dc13a0396a19ce371b0a63402
Administrator <admin@local.host>
2016-01-17 05:17:29 -0200

    Update

--- KickassSeries/Activator/DMGHandler/DamageHandle.cs
+++ KickassSeries/Activator/DMGHandler/DamageHandle.cs
@@ -22,67 +22,14 @@ namespace KickassSeries.Activator.DMGHandler
 
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if(!Settings.ConsiderAttacks)return;
-            var hero = sender as AIHeroClient;
-            if (hero == null) return;
-            if(hero.IsAlly) return;
 
-            if (args.Target.IsMe)
-            {
-                ReceivingAA = true;
-                Core.DelayAction(() => ReceivingAA = false, 80);
-            }
         }
 
         private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            var hero = sender as AIHeroClient;
-            if (hero == null || hero.IsAlly) return;
-
-            var dangerousspell =
-                DangerousSpells.Spells.FirstOrDefault(
-                    x =>
-                        x.Hero == hero.Hero && args.Slot == x.Slot &&
-                        Config.Types.SettingsMenu[x.Hero.ToString() + x.Slot].Cast<CheckBox>().CurrentValue);
-            //SkilShot
-            if (args.Target == null)
-            {
-                if (!Settings.ConsiderSkillshots) return;
-                if (args.Start.To2D().IsInRange(Player.Instance, args.SData.CastRangeDisplayOverride))
-                {
-                    var projection = Player.Instance.Position.To2D().ProjectOn(args.Start.To2D(), args.End.To2D());
-
-                    if (projection.IsOnSegment &&
-                        projection.SegmentPoint.Distance(Player.Instance.Position) <= args.SData.CastRadius + Player.Instance.BoundingRadius + 30)
-                    {
-                        if (dangerousspell != null)
-                        {
-                            ReceivingDangSpell = true;
-                            Core.DelayAction(() => ReceivingDangSpell = false, 80);
-                            return;
-                        }
-
-                        ReceivingSkillShot = true;
-                        Core.DelayAction(() => ReceivingSkillShot = false, 80);
-                    }
-                }
-            }
+           
             //Targetted spell
-            else
-            {
-                if (!Settings.ConsiderSpells) return;
-                if (args.Target.IsMe)
-                {
-                    ReceivingSpell = true;
-                    Core.DelayAction(() => ReceivingSpell = false, 80);
-                }
-                
-                if (dangerousspell != null && ReceivingSpell)
-                {
-                    ReceivingDangSpell = true;
-                    Core.DelayAction(() => ReceivingDangSpell = false, 80);
-                }
-            }        
+ 
         }
 
         #region Extensions
